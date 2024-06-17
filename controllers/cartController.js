ef88e0cbb5b13244a8e9ac599911ce749df3dd0b
tungbo <nguyenductung2k3@gmail.com>
2024-06-17 15:58:30 +0700

    tung

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,14 +1,14 @@
 const runQuery = require("../server");
 
 const createCart = async (cart) => {
-  const { idAccount, idPiza, quantity = 1, description } = cart;
-  const query = `INSERT INTO "Cart" ("idAccount", "idPiza", "quantity", "description") VALUES ($1, $2, $3) RETURNING *;`;
-  const res = await runQuery(query, [idAccount, idPiza, description]);
+  const { idAccount, idPiza, quantity, description } = cart;
+  const query = `INSERT INTO "Cart" ("idAccount", "idPiza", "quantity", "description") VALUES ($1, $2, $3, $4) RETURNING *;`;
+  const res = await runQuery(query, [idAccount, idPiza, quantity, description]);
   return res.rows[0];
 };
 
 const getCartByUserId = async (idAccount) => {
-  const query = `SELECT "imgPiza", "namePiza", "Price", "quantity", "description" FROM "Cart" 
+  const query = `SELECT "idCart", "imgPiza", "namePiza", "Price", "quantity", "description" FROM "Cart" 
                     JOIN "Piza" ON "Piza"."idPiza" = "Cart"."idPiza" 
                     JOIN "PizaSize" ON "Piza"."idSize" = "PizaSize"."idSize" 
                     WHERE "idAccount" = $1;`;
@@ -17,13 +17,22 @@ const getCartByUserId = async (idAccount) => {
 };
 
 const updateCart = async (cart) => {
-  const { idCart, quantity } = cart;
-
-  const query = `UPDATE "Cart" SET "quantity" = $1 WHERE "idCart" = $2 RETURNING *;`;
-  const res = await runQuery(query, [idCart, quantity]);
+  const { quantity, description, idCart } = cart;
+  const query = `UPDATE "Cart" SET "quantity" = $1, "description" = $2 WHERE "idCart" = $3 RETURNING *;`;
+  const res = await runQuery(query, [quantity, description, idCart]);
   return res.rows[0];
 };
+
+const deleteCart = async (cart) => {
+  const { idCart } = cart;
+  const query = `DELETE FROM "Cart" WHERE "idCart" = $1 RETURNING *;`;
+  const res = await runQuery(query, [idCart]);
+  return res.rows[0];
+};
+
 module.exports = {
   createCart,
   getCartByUserId,
+  updateCart,
+  deleteCart,
 };
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const {
   createCart,
   getCartByUserId,
+  updateCart,
+  deleteCart,
 } = require("../controllers/cartController");
 
 const router = express.Router();
 
 router.post("/cart", async (req, res) => {
   try {
-    const role = await createRole(req.body);
-    res.status(201).json(role);
+    const cart = await createCart(req.body);
+    res.status(201).json(cart);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -18,6 +20,24 @@ router.post("/cart", async (req, res) => {
 router.get("/cart/:idAccount", async (req, res) => {
   try {
     const cart = await getCartByUserId(req.params.idAccount);
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.put("/cart", async (req, res) => {
+  try {
+    const cart = await updateCart(req.body);
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/cart", async (req, res) => {
+  try {
+    const cart = await deleteCart(req.body);
     res.status(200).json(cart);
   } catch (err) {
     res.status(500).json({ error: err.message });
